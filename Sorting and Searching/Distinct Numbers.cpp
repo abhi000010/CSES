03f24f1b377adb1b61613aa2b8c6747171fe223e
abhi000010 <142973786+abhi000010@users.noreply.github.com>
2025-01-11 11:27:33 +0530

    Update Distinct Numbers.cpp
    added code

--- Sorting and Searching/Distinct Numbers.cpp
+++ Sorting and Searching/Distinct Numbers.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+void Solve() 
+{
+  int n;
+  cin>>n;
+  //here you can't use unorderd_setbecause of collisions
+  set<int>st;
+  for(int i=0;i<n;i++){
+    int ele;
+    cin>>ele;
+    st.insert(ele);
+  }
+   cout<<st.size()<<endl;
 
+
+}
+int main(){
+  ios_base::sync_with_stdio(0);
+    cin.tie(0);
+   
+       Solve();
+    
+  return 0;
+}
